1f4b2f82872e2cfde33e9366a2208795c95899e8
nick chen <nick.chen.pro@gmail.com>
2020-03-12 18:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  animal = 'cat'; 
+  animal = 'cat';
   return animal
 }
 
 function add2(n) {
-  return n + two
+  return n += two
 
   // Feel free to move things around!
   const two = 2
